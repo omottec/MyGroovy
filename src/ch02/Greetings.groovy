4ccf8ac4067265c90cebf930d1fd6f6e92317a3b
omottec <omottec@gmail.com>
2016-06-16 18:55:18 +0800

    create project structure

---
+++ src/ch02/Greetings.groovy
@@ -0,0 +1,8 @@
+public class Greetings {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("ho ")
+        }
+        System.out.print("Merry Groovy!")
+    }
+}
